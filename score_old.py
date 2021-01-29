24dd37b9ac946f4aef609ac7bd8937da0b8fd0df
ncharchenko <n_andree@hotmail.com>
2021-01-29 04:48:34 +0000

    Add files via upload

---
+++ score_old.py
@@ -0,0 +1,50 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+import json
+import logging
+import os
+import pickle
+import numpy as np
+import pandas as pd
+import joblib
+
+import azureml.automl.core
+from azureml.automl.core.shared import logging_utilities, log_server
+from inference_schema.schema_decorators import input_schema, output_schema
+from azureml.telemetry import INSTRUMENTATION_KEY
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+
+input_sample = pd.DataFrame({'golddiffat10': pd.Series([0], dtype="int64"), 'xpdiffat10': pd.Series([0], dtype="int64"), 'golddiffat15': pd.Series([0], dtype="int64"), 'xpdiffat15': pd.Series([0], dtype="int64")})
+
+try:
+    log_server.enable_telemetry(INSTRUMENTATION_KEY)
+    log_server.set_verbosity('DEBUG')
+    logger = logging.getLogger('azureml.automl.core.scoring_script')
+except:
+    pass
+
+
+def init():
+    global model
+    # This name is model.id of model that we want to deploy deserialize the model file back
+    # into a sklearn model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+    path = os.path.normpath(model_path)
+    path_split = path.split(os.sep)
+    log_server.update_custom_dimensions({'model_name': path_split[1], 'model_version': path_split[2]})
+    try:
+        logger.info("Loading model from path.")
+        model = joblib.load(model_path)
+        logger.info(type(model))
+        logger.info("Loading successful.")
+    except Exception as e:
+        logging_utilities.log_traceback(e, logger)
+        raise
+
+@input_schema('data', PandasParameterType(input_sample))
+@output_schema(NumpyParameterType(np.array([0])))
+def run(data):
+    result = model.predict(data)
+    return json.dumps({"result": result.tolist()})
